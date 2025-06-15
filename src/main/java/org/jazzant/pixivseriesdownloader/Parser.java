54eb50aecac032200cb2701be686217e696cbf4c
JazzAnt <jason.kwee98@gmail.com>
2025-06-15 23:54:34 +0700

    improve Parser class: break down large methods to many smaller ones so each only has a single responsibility, and rename many classes and variables to be more clear

--- src/main/java/org/jazzant/pixivseriesdownloader/Parser.java
+++ src/main/java/org/jazzant/pixivseriesdownloader/Parser.java
@@ -16,23 +16,23 @@ public class Parser {
     private int implicitWaitTime;
     private String seriesLink;
     private String chapterNameFormat;
-    //set by getSeriesDetailsFromSeriesPage()
+    //set by parseSeriesDetails()
     private String seriesTitle;
-    private String artist;
-    //set by getCurrentChapterDetails()
-    private String currentChapterLink;
-    private String currentChapterTitle;
-    private String currentChapterPixivID;
-    private String currentChapterUploadDate;
-    private int currentChapterNumber;
-    private int currentChapterPageAmount;
-    private ArrayList<String> currentChapterImageLinks;
-    private boolean isLatestChapter;
+    private String seriesArtist;
+    //set by parseChapterDetails()
+    private String currentLink;
+    private String chapterTitle;
+    private String chapterPixivID;
+    private String chapterUploadDate;
+    private int chapterNumber;
+    private int chapterPageAmount;
+    private ArrayList<String> chapterImageLinks;
 
     public Parser(){
         driver = new ChromeDriver();
         setImplicitWaitTime(15);
         chapterNameFormat = "";
+        chapterImageLinks = new ArrayList<>();
     }
 
     /**
@@ -45,108 +45,150 @@ public class Parser {
     }
 
     /**
-     * Goes to the series page.
+     * Attempts to go to the series link given in the parameter, and sets the series link if successful.
      * @param seriesLink The series link, the one that's formatted (pixiv.net/user/___/series/___)
      */
-    public void setSeriesFromLink(String seriesLink){
+    public void setSeries(String seriesLink){
+        driver.get(seriesLink);
         this.seriesLink = seriesLink;
-        driver.get(this.seriesLink);
-        currentChapterLink = "seriesPage";
-        isLatestChapter = false;
+        currentLink = seriesLink;
     }
 
     /**
      * Sets the formatting of the chapter
-     * @param chapterNameFormat
+     * @param chapterNameFormat the format of the chapter name
      */
     public void setChapterNameFormat(String chapterNameFormat){
         this.chapterNameFormat = chapterNameFormat;
     }
 
     /**
-     * Check if the current series exists, only use if the current page is the series page. Use to check if the artist
-     * had deleted this particular series. Works by checking for the existence of the "page not found" element.
+     * Check if the current series exists. Use to check if the artist had deleted this particular series.
+     * Works by checking for the existence of the "page not found" element.
      * @return true if the series exists, false if it's not.
      */
     public boolean seriesExists(){
-        return driver.findElements(By.className("sc-13hg6mj-1")).isEmpty();
+        return driver.findElements(By.className("eBICek")).isEmpty();
     }
 
     /**
-     * Get the series details from the series link, only use if the current page is the series page.
+     * Parses the series title and artist and sets their corresponding variables.
      */
-    public void getSeriesDetailsFromSeriesPage(){
-        seriesTitle = driver.findElement(By.className("sc-ad36b09f-1")).getText();
-        artist = driver.findElement(By.className("sc-jXbUNg")).findElement(By.tagName("div")).getText();
+    public void parseSeriesDetails(){
+        parseSeriesTitle();
+        parseSeriesArtist();
+    }
+
+    /**
+     * Parses the series title and sets its variable.
+     */
+    private void parseSeriesTitle(){
+        if(currentLink.equals(seriesLink))
+            seriesTitle = driver.findElement(By.className("eoyqMs")).getText();
+        else
+            seriesTitle = driver.findElement(By.className("bwGruY")).getText().split("#")[0];
+    }
+
+    /**
+     * Parses the series artist name and sets its variable.
+     */
+    private void parseSeriesArtist(){
+        seriesArtist = driver.findElement(By.className("kjDedB")).findElement(By.tagName("div")).getText();
     }
 
     /**
      * Goes to the next chapter, or the first chapter if the current page is the series page.
      */
     public void goToNextChapter(){
-        if(currentChapterLink.equals("seriesPage")){
-            currentChapterLink = PIXIV_URL + driver.findElement(By.className("sc-55fb7329-1")).getDomAttribute("href");
-        }
-        else {
-            currentChapterLink = PIXIV_URL + driver.findElement(By.className("gtm-series-next-work-button-in-illust-detail")).getDomAttribute("href");
-        }
-        driver.get(currentChapterLink);
+        if(currentLink.equals(seriesLink))
+            currentLink = PIXIV_URL + driver.findElement(By.className("gQCZLY")).getDomAttribute("href");
+        else
+            currentLink = PIXIV_URL + driver.findElement(By.className("gtm-series-next-work-button-in-illust-detail")).getDomAttribute("href");
+        driver.get(currentLink);
     }
 
     /**
-     * Fetches the current chapter's title, chapter number, chapter pixiv ID, page amount, image links, and checks whether this is
-     * the final chapter.
+     * Parses the current chapter's title, chapter number, chapter pixiv ID, page amount, image links and sets
+     * them to their respective variables.
      */
-    public void getCurrentChapterDetails(){
-        //Temporary variables to store stuff, also clears the image link list to be filled
-        String temp;
-        String[] tempArray;
-        List<WebElement> tempList;
-        currentChapterImageLinks.clear();
-        //Find chapter title
-        currentChapterTitle = driver.findElement(By.className("sc-e892487e-3")).getText();
-        //Find chapter pixiv ID
-        tempArray = currentChapterLink.split("/");
-        currentChapterPixivID = tempArray[tempArray.length-1];
-        //Find chapter upload date
-        currentChapterUploadDate = driver.findElement(By.tagName("time")).getDomAttribute("datetime").split("T")[0];
-        //Find chapter number
-        temp = driver.findElement(By.className("sc-e892487e-15")).getText();
-        currentChapterNumber = temp.charAt(temp.length()-1) - '0';
-        //Find chapter page count, the timeout wait is set to 0, as this also checks if the page
-        //counter even exists, because it doesn't exist for single page chapters
+    public void parseChapterDetails(){
+        parseChapterTitle();
+        parseChapterID();
+        parseChapterUploadDate();
+        parseChapterNumber();
+        parseChapterPageCount();
+        parseImageLinks();
+    }
+
+    /**
+     * Parses the current chapter title and sets its variable.
+     */
+    private void parseChapterTitle(){
+        chapterTitle = driver.findElement(By.className("dMvYWS")).getText();
+    }
+
+    /**
+     * Parses the current chapter ID and sets it's variable.
+     */
+    private void parseChapterID(){
+        String[] tempArray = currentLink.split("/");
+        chapterPixivID = tempArray[tempArray.length-1];
+    }
+
+    /**
+     * Parses the current chapter upload date and sets it's variable
+     */
+    private void parseChapterUploadDate(){
+        chapterUploadDate = driver.findElement(By.tagName("time")).getDomAttribute("datetime").split("T")[0];
+    }
+
+    /**
+     * Parses the current chapter number and sets it's variable
+     */
+    private void parseChapterNumber(){
+        String temp = driver.findElement(By.className("bwGruY")).getText();
+        chapterNumber = temp.charAt(temp.length()-1) - '0';
+    }
+
+    /**
+     * Parses the current chapter page count and sets it's variable
+     */
+    private void parseChapterPageCount(){
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(0));
-        tempList = driver.findElements(By.className("gtm-manga-viewer-open-preview"));
+        List<WebElement> tempList = driver.findElements(By.className("gtm-manga-viewer-open-preview"));
         if(!tempList.isEmpty()){
-            temp = tempList.get(0).findElement(By.tagName("span")).getText();
-            currentChapterPageAmount = temp.charAt(temp.length()-1) - '0';
+            String temp = tempList.get(0).findElement(By.tagName("span")).getText();
+            chapterPageAmount = temp.charAt(temp.length()-1) - '0';
         }
-        else {
-            currentChapterPageAmount = 1;
-        }
-        //Get the image link and split it in two
-        temp = driver.findElement(By.className("sc-e1dc2ae6-1")).getDomAttribute("src");
-        tempArray = temp.split("0_master1200");
-        //Fill the image links list, using the above image link and inserting the page number in between each
-        //because that's how pixiv links are formatted
-        for(int page=0; page<currentChapterPageAmount; page++){
-            temp = tempArray[0] + page + tempArray[1];
-            currentChapterImageLinks.add(temp);
-        }
-        //checks if this is the latest chapter, then finally return the implicit wait to the default
-        isLatestChapter = driver.findElements(By.className("gtm-series-next-work-button-in-illust-detail")).isEmpty();
+        else
+            chapterPageAmount = 1;
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(implicitWaitTime));
     }
 
     /**
-     * Checks if the current chapter is the latest chapter. getCurrentChapterDetails does this too but this one won't
-     * bother checking the other details. Use when trying to update an existing library to check if any new chapters have
-     * arrived.
+     * Parses the current chapter image download links and sets it's variable
+     */
+    private void parseImageLinks(){
+        chapterImageLinks.clear();
+        //Get the image link and split it in two
+        String temp = driver.findElement(By.className("feuJAv")).getDomAttribute("src");
+        String[] tempArray = temp.split("0_master1200");
+        //Fill the image links list, using the above image link and inserting the page number in between each
+        //because that's how pixiv links are formatted
+        for(int page = 0; page< chapterPageAmount; page++){
+            temp = tempArray[0] + page + tempArray[1];
+            chapterImageLinks.add(temp);
+        }
+    }
+
+    /**
+     * Checks if the parser is currently in the latest chapter.
      * @return true if it's the latest chapter, and false if not.
      */
     public boolean isLatestChapter(){
+        if(currentLink.equals(seriesLink)) return false;
         driver.manage().timeouts().implicitlyWait(Duration.ZERO);
-        isLatestChapter = driver.findElements(By.className("gtm-series-next-work-button-in-illust-detail")).isEmpty();
+        boolean isLatestChapter = driver.findElements(By.className("gtm-series-next-work-button-in-illust-detail")).isEmpty();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
         return isLatestChapter;
     }
@@ -156,7 +198,7 @@ public class Parser {
      * can be given a formatter to make a custom filename. The formatter must contain either the chapter title, number,
      * id, or upload date or else it will use the default format (because if it doesn't contain at least one of the
      * aforementioned parameters, it might end up using the same name for every chapter).
-     * @return formatted filename
+     * @return formatted filename if it meets the formatting requirements, and the default filename otherwise
      */
     public String generateFileName(){
         if(chapterNameFormat.contains("{chapter_title}")
@@ -164,14 +206,14 @@ public class Parser {
         || chapterNameFormat.contains("{chapter_id}")
         || chapterNameFormat.contains("{upload_date}"))
             return chapterNameFormat
-               .replace("{chapter_title}", currentChapterTitle)
-               .replace("{chapter_number}", currentChapterNumber+"")
-               .replace("{chapter_id}", currentChapterPixivID)
-               .replace("{upload_date}", currentChapterUploadDate)
-               .replace("{page_amount}", currentChapterPageAmount+"")
-               .replace("{artist}", artist)
+               .replace("{chapter_title}", chapterTitle)
+               .replace("{chapter_number}", chapterNumber +"")
+               .replace("{chapter_id}", chapterPixivID)
+               .replace("{upload_date}", chapterUploadDate)
+               .replace("{page_amount}", chapterPageAmount +"")
+               .replace("{artist}", seriesArtist)
                .replace("{series_title}", seriesTitle);
 
-        return "Chapter" + currentChapterNumber + "_" + currentChapterTitle;
+        return "Chapter" + chapterNumber + "_" + chapterTitle;
     }
 }
