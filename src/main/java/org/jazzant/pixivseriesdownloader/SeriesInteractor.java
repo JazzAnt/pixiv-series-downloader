0c5bd6323dd2264290536669f626e979dc39b512
JazzAnt <jason.kwee98@gmail.com>
2025-06-27 15:12:08 +0700

    add placeholder saveSeries method to interactor for testing purposes (will be replaced with actual save to database once the DAO is implemented)

--- src/main/java/org/jazzant/pixivseriesdownloader/SeriesInteractor.java
+++ src/main/java/org/jazzant/pixivseriesdownloader/SeriesInteractor.java
@@ -6,4 +6,16 @@ public class SeriesInteractor {
     public SeriesInteractor(SeriesModel model){
         this.model = model;
     }
+
+    public void saveSeries(){
+        System.out.println("Saving Series to Database: "
+                + "\nDir Group: " + model.getDirectoryGroup()
+                + "\nDir Title: " + model.getDirectoryTitle()
+                + "\nTitle: " + model.getTitle()
+                + "\nArtist: " + model.getArtist()
+                + "\nStatus: " + model.getStatus()
+                + "\nArtistID: " + model.getArtistId()
+                + "\nSeriesID: " + model.getSeriesId()
+                + "\nLatestChapterID: " + model.getLatestChapterId());
+    }
 }
