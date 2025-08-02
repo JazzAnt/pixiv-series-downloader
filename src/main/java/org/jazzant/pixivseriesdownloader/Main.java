3ff011cca2eac6c07d20b9bcd476f3980d86d6db
JazzAnt <jason.kwee98@gmail.com>
2025-08-02 18:24:32 +0700

    add prompt for user to configure the config file at startup if no existing config file is detected

--- src/main/java/org/jazzant/pixivseriesdownloader/Main.java
+++ src/main/java/org/jazzant/pixivseriesdownloader/Main.java
@@ -5,6 +5,7 @@ import javafx.application.Platform;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -13,10 +14,18 @@ public class Main extends Application {
     private final SeriesBroker broker = new SeriesBroker();
     private final Parser parser = new Parser();
     private final Downloader downloader = new Downloader();
+    private final ConfigManager configManager = new ConfigManager();
     public static void main(String[] args) {launch(args);}
 
     @Override
     public void start(Stage stage) throws IOException {
+        if(!configManager.configExists()) createConfig();
+
+        String libraryDir = configManager.getProperty(configManager.KEY_LIBRARY);
+        SaveAs saveAs = SaveAs.valueOf(configManager.getProperty(configManager.KEY_SAVEAS));
+        downloader.setLibraryDir(libraryDir);
+        downloader.setFileFormat(saveAs);
+
         stage.setOnCloseRequest(windowEvent -> {
             parser.quit();
             Platform.exit();
@@ -34,5 +43,31 @@ public class Main extends Application {
         stage.setTitle("Pixiv Series Downloader");
         stage.setScene(scene);
         stage.show();
+    }
+
+    private void createConfig() throws IOException {
+        configManager.createConfigFile();
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("config-view.fxml"));
+        Parent root = loader.load();
+        Scene scene = new Scene(root);
+        ConfigController controller = loader.getController();
+        controller.setConfigManager(configManager);
+        controller.setLibrary(System.getProperty("user.home") + "/Library");
+        controller.setInfoText("Welcome to Pixiv Series Manager! " +
+                "Please enter the following settings for the program to work. " +
+                "(these settings can be changed later but they won't change " +
+                "any existing files you've already downloaded)");
+
+        Stage stage = new Stage();
+        stage.setOnCloseRequest(windowEvent -> {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setContentText("Program Cannot Run Without the Config Being Set!");
+            alert.showAndWait();
+            parser.quit();
+            Platform.exit();
+        });
+        stage.setTitle("First Time User Configuration");
+        stage.setScene(scene);
+        stage.showAndWait();
     }
 }
